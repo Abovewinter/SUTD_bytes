c0d8816719363097aee0dcd3c09691d22620e7eb
Lai-JunJie <junjie1337@gmail.com>
2025-04-08 12:25:47 +0800

    added blank activities for each navigation icon
    Updated order activity, have not implemented features yet.
    Added navigation class that can be used across all activity other than login and registration.

---
+++ app/src/main/java/com/example/myapplication/FoodClicker.java
@@ -0,0 +1,5 @@
+package com.example.myapplication;
+
+public interface FoodClicker {
+    void onFoodClick(String foodname);
+}
--- app/src/main/java/com/example/myapplication/UserProfileActivity.java
+++ app/src/main/java/com/example/myapplication/UserProfileActivity.java
@@ -2,7 +2,6 @@ package com.example.myapplication;
 
 import android.app.Activity;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,24 +9,13 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class OrderActivity extends Activity {
+public class UserProfileActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_order);
-        Navigation.setupNavigation(this);
-
-        Button btn_koi = findViewById(R.id.koi_home);
-        Button btn_wokhey = findViewById(R.id.wokhey_home);
-        Button btn_fastfood = findViewById(R.id.btn_fastfood);
-        Button btn_dessert = findViewById(R.id.btn_dessert);
-        Button btn_maincourse = findViewById(R.id.btn_maincourse);
-        Button btn_others = findViewById(R.id.btn_others);
-        Button btn_mc = findViewById(R.id.mc_home);
-
-        
+        setContentView(R.layout.activity_user_profile);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
