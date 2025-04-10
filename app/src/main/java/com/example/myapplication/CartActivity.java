d4901a1f20d7dffc94588f7da653986a49e57b5d
Samaralau <samaralauyx@gmail.com>
2025-04-10 11:42:12 +0800

    update order

--- app/src/main/java/com/example/myapplication/CartActivity.java
+++ app/src/main/java/com/example/myapplication/CartActivity.java
@@ -20,6 +20,8 @@ import java.util.List;
 
 public class CartActivity extends Activity {
     int quantity1 = 1;
+    double deliveryFee = 1.50;
+    String redeem_distance = "short distance";
     List<Double> prices1 = Arrays.asList(5.8,3.5,5.5);
     List<Double> prices2 = Arrays.asList(4.9,2.2,6.0);
     CartItem cartItems;
@@ -39,6 +41,8 @@ public class CartActivity extends Activity {
 
         TextView storename = findViewById(R.id.store_name_cart);
         TextView foodname = findViewById(R.id.food_name_cart);
+        TextView deliveryprice = findViewById(R.id.delivery_fee_cart);
+        TextView distance = findViewById(R.id.reedeming_distance_cart);
         TextView price = findViewById(R.id.food_price_cart);
         TextView totalprice = findViewById(R.id.total_price_cart);
 
@@ -48,13 +52,13 @@ public class CartActivity extends Activity {
 
             storename.setText(firstItem.getStoreName());
             foodname.setText(firstItem.getFoodName());
+            deliveryprice.setText(String.format("$%.2f", deliveryFee));
+            distance.setText(redeem_distance);
             quantity.setText(firstItem.getQuantity() + "x");
             price.setText("$" + String.format("%.2f", firstItem.getTotalPrice()));
 
             // Calculate total + delivery fee
-            double deliveryFee = 2.50;
             double total = firstItem.getTotalPrice() + deliveryFee;
-
             totalprice.setText("$" + String.format("%.2f", total));
 
         }
@@ -69,7 +73,6 @@ public class CartActivity extends Activity {
                 double pricing = quantity1 * prices1.get(0);
                 price.setText("$" + pricing);
 
-                double deliveryFee = 2.50;
                 double total = quantity1 * prices1.get(0) + deliveryFee;
                 totalprice.setText("$" + total);
             }
@@ -99,6 +102,7 @@ public class CartActivity extends Activity {
        // TextView totalPrice = findViewById(R.id.total_price_cart);
        // double total = quantity1 * option.getPrices().get(0) + quantity2 * option.getPrices().get(1);
        // totalPrice.setText((String.format("$%.2f", total)));
+        
 
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
